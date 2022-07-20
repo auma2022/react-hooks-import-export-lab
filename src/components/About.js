f1ed564f650622e84237ed057962a3bc742fc409
auma2022 <auma.amwayi@student.moringaschool.com>
2022-07-20 11:50:04 +0300

    add comments

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,50 @@
 import React from "react";
 import { image } from "../data/user";
+/*
+ The function itself is just `returning` the html string you see in the very first line
+2. Then it's calling the rendering engine (react) to render the html string in the main.js and return a div root node
+    (div id="about")
+3. Then, the div node is being appended to the body (document.getElementById('body').appendChild(div))
+4. Finally, react is rendering the div (now with the content) on the page
 
+## Why React?
+React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.
+
+## Getting Started
+The best way to get started with React is to start over and write a brand new app.
+React offers a simple way to create a new app in a single file, and you can use it for 
+most new projects. In addition to the React library, you’ll need to install some additional npm packages. These are:
+
+- react-dom - A library that can be used to render React components, attach them to the DOM, and manipulate the DOM in
+    response to application state changes.
+- babel-preset-react - A preset that includes Babel plugins needed to compile React JSX code
+- babel-preset-es2015 - A preset that configures Babel for you to use ES2015 features today, the CommonJS API, and more.
+
+So run this command in terminal:
+```bash
+npm install -g create-react-app
+npm install -g babel-preset-react
+npm install -g babel-preset-es2015
+```
+
+Then run this command in terminal to create a new app:
+```bash
+create-react-app my-app
+```
+
+Now, you'll find a directory named my-app in the current working directory that contains the following:
+```bash
+my-app/
+├── my-app.css  
+├── my-app.js   
+├── public      
+│   ├── index.html         
+│   └── manifest.json      
+├── src      
+│   ├── App.css             
+│   ├── App.test.js         
+│  
+*/ 
 function About() {
   return (
     <div id="about">
--- src/data/user.js
+++ src/data/user.js
@@ -1,3 +1,8 @@
+/*
+ defines three variables: username, city, image.
+2. assigns variable values: 'Liza', 'New York', 'https://i.imgur.com/mV8PQxj.gif'
+3. exports username, city, image as named exports
+4. exports the three variables in the form of a single object */
 const username = "Liza";
 const city = "New York";
 
